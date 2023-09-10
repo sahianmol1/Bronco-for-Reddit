91c588619d61871f035ac41ecc95e9a79a61ae00
Anmol <sahi.anmool@gmail.com>
2023-09-10 12:10:14 +0530

    add Bottom Nav bar

--- app/src/main/java/com/bestway/broncoforreddit/MainActivity.kt
+++ app/src/main/java/com/bestway/broncoforreddit/MainActivity.kt
@@ -9,11 +9,28 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.List
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Tab
@@ -23,15 +40,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.bestway.broncoforreddit.components.PostWidget
 import com.bestway.broncoforreddit.data.mock.listOfMockedPosts
+import com.bestway.broncoforreddit.ui.model.BottomNav
 import com.bestway.broncoforreddit.ui.theme.BroncoForRedditTheme
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalFoundationApi::class)
+    @OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
@@ -46,14 +65,48 @@ class MainActivity : ComponentActivity() {
                 mutableStateOf(
                     listOf(
                         "Hot",
-                        "Best",
                         "New",
+                        "Top",
+                        "Best",
                         "Rising",
-                        "Controversial",
-                        "Top"
+                        "Controversial"
                     )
                 )
             }
+
+            val bottomNavs by remember {
+                mutableStateOf(
+                    listOf(
+                        BottomNav(
+                            title = "Home",
+                            selectedIcon = Icons.Filled.Home,
+                            unselectedIcon = Icons.Outlined.Home
+                        ),
+                        BottomNav(
+                            title = "Search",
+                            selectedIcon = Icons.Filled.Search,
+                            unselectedIcon = Icons.Outlined.Search
+                        ),
+                        BottomNav(
+                            title = "Create",
+                            selectedIcon = Icons.Filled.Add,
+                            unselectedIcon = Icons.Outlined.Add
+                        ),
+                        BottomNav(
+                            title = "Subs",
+                            selectedIcon = Icons.Filled.List,
+                            unselectedIcon = Icons.Outlined.List
+                        ),
+                        BottomNav(
+                            title = "About",
+                            selectedIcon = Icons.Filled.Info,
+                            unselectedIcon = Icons.Outlined.Info
+                        ),
+                    )
+                )
+            }
+
+            var selectedBottomNavIndex by rememberSaveable { mutableStateOf(0) }
 
             BroncoForRedditTheme {
                 // A surface container using the 'background' color from the theme
@@ -61,57 +114,87 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .statusBarsPadding()
-                    ) {
-                        ScrollableTabRow(
-                            selectedTabIndex = pagerState.currentPage
-                        ) {
-                            tabs.forEachIndexed { index, title ->
-                                Tab(
-                                    text = { Text(text = title) },
-                                    selected = pagerState.currentPage == index,
-                                    onClick = {
-                                        coroutineScope.launch {
-                                            pagerState.animateScrollToPage(index)
+                    Scaffold(
+                        bottomBar = {
+                            NavigationBar {
+                                bottomNavs.forEachIndexed { index, bottomNav ->
+                                    NavigationBarItem(
+                                        selected = selectedBottomNavIndex == index,
+                                        onClick = {
+                                            selectedBottomNavIndex = index
+                                        },
+                                        icon = {
+                                            Icon(
+                                                imageVector = if (selectedBottomNavIndex == index) {
+                                                    bottomNav.selectedIcon
+                                                } else {
+                                                    bottomNav.unselectedIcon
+                                                },
+                                                contentDescription = "Bottom Bar: ${bottomNav.title}"
+                                            )
+
+                                        },
+                                        label = {
+                                            Text(text = bottomNav.title)
                                         }
-                                    }
-                                )
+                                    )
+                                }
                             }
                         }
-                        HorizontalPager(
-                            modifier = Modifier.fillMaxSize(),
-                            pageCount = tabs.size,
-                            state = pagerState
-                        ) { page ->
-                            when (page) {
-                                0, 1, 2, 5 -> {
-                                    LazyColumn {
-                                        items(
-                                            count = listSize,
-                                            key = { index -> list[index].id }
-                                        ) { index ->
-                                            PostWidget(
-                                                modifier = when (index) {
-                                                    lastElementOfList -> Modifier.navigationBarsPadding()
-                                                    else -> Modifier
-                                                }
-                                            )
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .statusBarsPadding()
+                                .padding(it)
+                        ) {
+                            ScrollableTabRow(
+                                selectedTabIndex = pagerState.currentPage
+                            ) {
+                                tabs.forEachIndexed { index, title ->
+                                    Tab(
+                                        text = { Text(text = title) },
+                                        selected = pagerState.currentPage == index,
+                                        onClick = {
+                                            coroutineScope.launch {
+                                                pagerState.animateScrollToPage(index)
+                                            }
+                                        }
+                                    )
+                                }
+                            }
+                            HorizontalPager(
+                                modifier = Modifier.fillMaxSize(),
+                                pageCount = tabs.size,
+                                state = pagerState
+                            ) { page ->
+                                when (page) {
+                                    0, 1, 2, 5 -> {
+                                        LazyColumn {
+                                            items(
+                                                count = listSize,
+                                                key = { index -> list[index].id }
+                                            ) { index ->
+                                                PostWidget(
+                                                    modifier = when (index) {
+                                                        lastElementOfList -> Modifier.navigationBarsPadding()
+                                                        else -> Modifier
+                                                    }
+                                                )
+                                            }
                                         }
                                     }
-                                }
 
-                                else -> {
-                                    Column(
-                                        modifier = Modifier.fillMaxSize(),
-                                        verticalArrangement = Arrangement.Center,
-                                        horizontalAlignment = Alignment.CenterHorizontally
-                                    ) {
-                                        Text(
-                                            text = "Hello World"
-                                        )
+                                    else -> {
+                                        Column(
+                                            modifier = Modifier.fillMaxSize(),
+                                            verticalArrangement = Arrangement.Center,
+                                            horizontalAlignment = Alignment.CenterHorizontally
+                                        ) {
+                                            Text(
+                                                text = "Hello World"
+                                            )
+                                        }
                                     }
                                 }
                             }
---
+++ app/src/main/java/com/bestway/broncoforreddit/ui/model/BottomNav.kt
@@ -0,0 +1,9 @@
+package com.bestway.broncoforreddit.ui.model
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomNav(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector
+)
