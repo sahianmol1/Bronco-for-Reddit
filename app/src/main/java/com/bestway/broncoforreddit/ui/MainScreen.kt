e889f2c12578dcc0c68a4836cf7cc28d0c454e1c
anmolsinghsahi <sahi.anmool@gmail.com>
2023-10-15 22:42:46 +0530

    add navigation bar color as same as bottom navigation bar

--- app/src/main/java/com/bestway/broncoforreddit/ui/MainScreen.kt
+++ app/src/main/java/com/bestway/broncoforreddit/ui/MainScreen.kt
@@ -1,13 +1,19 @@
 package com.bestway.broncoforreddit.ui
 
+import android.app.Activity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.bestway.broncoforreddit.navigation.BRNavHost
 import com.bestway.broncoforreddit.ui.components.BRNavigationBar
@@ -17,6 +23,16 @@ fun MainScreen(
     navController: NavHostController
 ) {
     val context = LocalContext.current
+
+    val view = LocalView.current
+    val navigationBarColor = MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp)
+
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.navigationBarColor = navigationBarColor.toArgb()
+        }
+    }
 
     Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
         Scaffold(
