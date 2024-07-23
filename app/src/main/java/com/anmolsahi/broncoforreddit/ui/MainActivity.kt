6ab7d7d184349ea600bc75568ac4757baa24693c
anmolsinghsahi <sahi.anmool@gmail.com>
2024-07-23 21:12:06 +0530

    pre-commit check - 5

--- app/src/main/java/com/anmolsahi/broncoforreddit/ui/MainActivity.kt
+++ app/src/main/java/com/anmolsahi/broncoforreddit/ui/MainActivity.kt
@@ -21,7 +21,5 @@ class MainActivity : ComponentActivity() {
             BroncoForRedditTheme {
                 val navController = rememberNavController()
                 MainScreen(navController = navController)
-            }
-        }
-    }
+            } } }
 }
