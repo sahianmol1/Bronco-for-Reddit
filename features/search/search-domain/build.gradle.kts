7918759a88b1d822fec62a96ca208baa180b9e99
anmolsinghsahi <sahi.anmool@gmail.com>
2024-08-29 08:36:12 +0530

    add workflow file to run unit tests

--- features/search/search-domain/build.gradle.kts
+++ features/search/search-domain/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     id("java-library")
@@ -9,6 +11,13 @@ java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
+
 dependencies {
 
     // Unit test
