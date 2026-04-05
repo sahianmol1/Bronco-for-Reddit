fc66f55a503dc73acacff16d41618cdd501a0304
sahianmol1 <sahi.anmool@gmail.com>
2026-04-05 13:45:35 +0530

    Remove @Suppress("DSL_SCOPE_VIOLATION")

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -1,6 +1,5 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     id("java-library")
     alias(libs.plugins.org.jetbrains.kotlin.jvm)
